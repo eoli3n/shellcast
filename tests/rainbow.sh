922a3532be17f7961538019398f5fecfaa2f74ce
eoli3n <jonathan.kirszling@runbox.com>
2024-12-28 00:01:40 +0100

    updated config-sample.yml, TODO, and rainbow script

--- tests/rainbow.sh
+++ tests/rainbow.sh
@@ -10,9 +10,7 @@ CYAN='\033[0;36m'
 WHITE='\033[0;37m'
 RESET='\033[0m'
 
-for i in {0..20..1}
+for i in {0..20000..1}
 do
     echo -e "$i ${WHITE}white${RESET} hide ${RED}red${RESET} hide ${GREEN}green${RESET} hide ${YELLOW}yellow${RESET} hide ${BLUE}blue${RESET} hide ${MAGENTA}magenta${RESET} hide ${CYAN}cyan${RESET}"
-    echo -e "${YELLOW}...lets sleep 0.$i s${RESET}"
-    sleep 0.$i
 done
