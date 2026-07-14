123506ba53dbfb87e50f77a6b268da3d5ed6db29
eoli3n <jonathan.kirszling@runbox.com>
2026-07-14 12:24:56 +0200

    Implement auth_request, password, basicauth authentications with hashed passwords, service mode restriction, improve logging and add tests

--- shellcast.js
+++ shellcast.js
@@ -10,12 +10,14 @@ const express = require('express'),
       server = http.createServer(app),
       subdir = "/" + process.env.SUBDIR,
       { Server } = require('socket.io'),
-      io = new Server(server, { path: subdir + '/socket.io' }),
+      io = new Server(server , /*{ cors: { origin: "http://localhost:3000/shellcast/rainbow", credentials: true }},*/  { path: subdir + '/socket.io' }),
       yaml = require('js-yaml'),
       morgan = require('morgan'),
       path = require('path'),
       favicon = require('serve-favicon'),
-      validator = require('validator');
+      validator = require('validator'),
+      basicAuth = require('express-basic-auth'),
+      { exec } = require('child_process');
 
 // Set trust proxy before adding any middleware or routes
 app.set('trust proxy', true);
@@ -26,20 +28,81 @@ app.set('view engine', 'html');
 app.set('views', __dirname + '/views/');
 app.use(subdir, express.static(path.join(__dirname, '/public')));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(morgan('combined'));
 
 // Configure morgan logs
-morgan.token("user", (req) => req.headers["x-remote-user"] || "-");
-morgan.token("group", (req) => req.headers["x-group"] || "-");
-app.use(morgan(':remote-addr - :user :group [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length]'));
+morgan.token("auth", (req) => {
+    return req.authlog || "-";
+});
+morgan.token('status-text', (req, res) => {
+    const status = res.statusCode;
+
+    const messages = {
+        200: 'OK',
+        201: 'Created',
+        204: 'No Content',
+        400: 'Bad Request',
+        401: 'Unauthorized',
+        403: 'Forbidden',
+        404: 'Not Found',
+        500: 'Internal Server Error'
+    };
+
+    return messages[status] || 'Unknown';
+});
+
+app.use(morgan(':remote-addr - :auth [:date[clf]] ":method :url HTTP/:http-version" :status :status-text :response-time ms'));
 
 // Load YAML config
 let config;
+
 try {
     config = yaml.safeLoad(fs.readFileSync(process.argv[2], 'utf8'));
 } catch (error) {
     console.error('Error loading YAML config:', error);
     process.exit(1);
+}
+
+function loadUsers() {
+    try {
+        const users = yaml.safeLoad(fs.readFileSync("users.yml", "utf8"));
+
+        if (!users || typeof users !== "object" || Array.isArray(users)) {
+            throw new Error("users.yml must contain an object");
+        }
+
+        for (const [username, passwordHash] of Object.entries(users)) {
+            if (typeof username !== "string" || typeof passwordHash !== "string") {
+                throw new Error(`Invalid user entry: ${username}`);
+            }
+        }
+
+        return users;
+
+    } catch (error) {
+        if (error.code === "ENOENT") {
+            console.log("No users.yml found, local authentication disabled.");
+            return {};
+        }
+
+        console.error("Unable to load users.yml:", error.message);
+        process.exit(1);
+    }
+}
+
+const users = loadUsers();
+// DEBUG
+//console.log(users);
+
+const bcrypt = require("bcrypt");
+
+function checkUser(username, password) {
+    const storedHash = users[username];
+
+    if (typeof storedHash !== "string") {
+        return false;
+    }
+
+    return bcrypt.compareSync(password, storedHash);
 }
 
 const forbiddenChars = ['>', '<', '|', '&', ';', '(', ')', '\\', '!', '*', '$', '=', '+', '~', '"', ' '];
@@ -104,9 +167,10 @@ io.sockets.on('connection', (socket) => {
         let castArgs = [];
         let cmd = '';
         let castHighlightJson = [];
+              
         
         // Find the cast corresponding to the URL
-        const cast = config.find(c => c.url.replace(/\/$/, '') === url.replace(/\/$/, ''));
+        const cast = config.find(c => c.url.replace(/\/$/, '') === url[0].replace(/\/$/, ''));
         
         if (cast) {
             cmd = cast.cmd;
@@ -245,66 +309,152 @@ io.sockets.on('connection', (socket) => {
     });
 });
 
+// BasicAuth permettant aux utilisateurs locaux de se connecter
+const basicAuthShellcast = basicAuth({
+    authorizer: checkUser,
+    challenge: true,
+    unauthorizedResponse: () => {
+        return "Unauthorized";
+    },
+    realm: "shellcast"
+});
+
+// Middleware d'authentification
+function authIfNeeded(service) {
+    return (req, res, next) =>{
+
+        // Si grant est absent du service
+        if (!service.grant) {
+            return next();
+        }
+
+        // Si x-remote-user est autorisé
+        const remoteUser = req.headers["x-remote-user"];
+
+        if (
+            remoteUser &&
+            Array.isArray(service.grant.x_remote_user) &&
+            service.grant.x_remote_user.includes(remoteUser)
+        ) {
+            req.authlog = "x_remote_user=" + remoteUser;
+            return next();
+        }
+
+        // Si x-group est autorisé
+        const group = req.headers["x-group"];
+
+        if (
+            group &&
+            Array.isArray(service.grant.x_group) &&
+            service.grant.x_group.includes(group)
+        ) {
+            req.authlog = "x_group=" + group;
+            return next();
+        }
+
+        // Si password est autorisé
+        const password = req.query.password;
+
+        if (
+            typeof password === "string" &&
+            Array.isArray(service.grant.password)
+        ) {
+            for (const entry of service.grant.password) {
+
+                const [tag, hash] = Object.entries(entry)[0];
+
+                if (bcrypt.compareSync(password, hash)) {
+                    req.authlog = "password=" + tag;
+                    return next();
+                }
+            }
+        }
+
+        // Si basic_auth activé via grant.local_user
+        if (service.grant.local_user !== undefined) {
+
+            // Si utilisateur authentifié
+            return basicAuthShellcast(req, res, () => {
+
+                req.authlog = "local_user=" + req.auth.user;
+
+                // Si utilisateur authentifié autorisé sur le service
+                if (service.grant.local_user.includes(req.auth.user)) {
+                    return next();
+                }
+
+                return res.sendStatus(403);
+            });
+        }
+
+        // Sinon erreur d'accès
+        return res.sendStatus(401);
+    }
+}
+
 // Handle HTTP requests
 config.forEach((cast) => {
     cast.url = subdir + cast.url.replace(/\/$/, '');
     
-    app.get(cast.url, (req, res) => {
-        res.setHeader('Content-Type', 'text/plain');
-        if (cast.password && cast.password !== req.query.password) {
-            return res.status(403).send('Missing or wrong password...');
-        }
-        const errors = validateParams(cast.args || [], req, res, cast);
-        if (errors.length > 0) {
-            return res.status(400).send(errors.join('<br>'));
-        }
-        res.setHeader('Content-Type', 'text/html');
-        res.render('index', { title: cast.name, subdir: subdir });
-    });
+    // Si mode web ou non défini, activer cette route
+    if (cast.mode === undefined || cast.mode === "web") {
+        app.get(cast.url, authIfNeeded(cast), (req, res) => {       
+            // Renvoie la liste des paramètres incorrect au sein du service lancé et renvoie une erreur 400 côté client si la liste en contient au moins une 
+            const errors = validateParams(cast.args || [], req, res, cast);
+            if (errors.length > 0) {
+                return res.status(400).send(errors.join('<br>'));
+            }
+            // Charge la page html où sera affiché les résultats de la commande
+            res.setHeader('Content-Type', 'text/html');
+            res.render('index', { title: cast.name, subdir: subdir });
+        });
+    }
 
-    app.get(cast.url + '/plain', (req, res) => {
-        res.setHeader('Content-Type', 'text/plain');
-        if (cast.password && cast.password !== req.query.password) {
-            return res.status(403).send('Incorrect or missing password...');
-        }
-        const errors = validateParams(cast.args || [], req, res, cast);
-        if (errors.length > 0) {
-            return res.status(400).send(errors.join('<br>'));
-        }
+    // Si mode plain ou non défini, activer cette route
+    if (cast.mode === undefined || cast.mode === "plain") {
+        app.get(cast.url + '/plain' ,authIfNeeded(cast) ,(req, res) => {
+            res.setHeader('Content-Type', 'text/plain');
+            // Renvoie la liste des paramètres incorrect et renvoie une erreur 400 côté client si la liste en contient au moins une 
+            const errors = validateParams(cast.args || [], req, res, cast);
+            if (errors.length > 0) {
+                return res.status(400).send(errors.join('<br>'));
+            }
 
-        let cmd = cast.cmd;
-        const castArgs = cast.args ? cast.args.map(arg => req.query[arg]) : [];
+            let cmd = cast.cmd;
+            const castArgs = cast.args ? cast.args.map(arg => req.query[arg]) : [];
 
-        //console.log("castArgs : " + castArgs)
-        
-        if (cast.args && cast.args.length > 0) {
-            castArgs.forEach((arg, index) => {
-                const placeholder = `{${cast.args[index]}}`;
-                cmd = cmd.split(placeholder).join(arg);
+            //console.log("castArgs : " + castArgs)
+            
+            if (cast.args && cast.args.length > 0) {
+                castArgs.forEach((arg, index) => {
+                    const placeholder = `{${cast.args[index]}}`;
+                    cmd = cmd.split(placeholder).join(arg);
+                });
+            }
+
+            // Add magic x_forwarded_for var
+            if (cmd.includes("{x_forwarded_for}")) {
+                let x_forwarded_for = req.ip;
+                cmd = cmd.split("{x_forwarded_for}").join(x_forwarded_for);
+                castArgs.push(x_forwarded_for);
+            }
+            // Permet d'exécuter des commandes produisant beaucoup de données
+            // et d'intéragir avec les sorties std
+            const run = spawn('bash', ['-c', cmd]);
+            // On exécute la commande sur stdout et stderr
+            run.stdout.pipe(res);
+            run.stderr.pipe(res);
+            
+            run.on('error', (error) => {
+                console.error('Error spawning process:', error);
+                res.status(500).send(`Error spawning process: ${error.message}`);
             });
-        }
-
-        // Add magic x_forwarded_for var
-        if (cmd.includes("{x_forwarded_for}")) {
-            let x_forwarded_for = req.ip;
-            cmd = cmd.split("{x_forwarded_for}").join(x_forwarded_for);
-            castArgs.push(x_forwarded_for);
-        }
-
-        const run = spawn('bash', ['-c', cmd]);
-
-        run.stdout.pipe(res);
-        run.stderr.pipe(res);
-        
-        run.on('error', (error) => {
-            console.error('Error spawning process:', error);
-            res.status(500).send(`Error spawning process: ${error.message}`);
+            
+            run.on('close', (code) => {
+                console.log(`Command ${cmd} exited with code ${code}`);
+            });
         });
-        
-        run.on('close', (code) => {
-            console.log(`Command ${cmd} exited with code ${code}`);
-        });
-    });
+    }
 });
 
 // Handle 404 errors
